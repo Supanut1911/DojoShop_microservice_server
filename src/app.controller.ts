aa6d5d0c3fdef353fe189507bd5d576a23f032f8
your_name <your_email>
2020-12-24 15:15:49 +0700

    set microservice

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
 import { AppService } from './app.service';
+
+interface IString {
+  message: string
+}
 
 @Controller()
 export class AppController {
+  private logger = new Logger('AppController')
+
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @GrpcMethod('AppController', 'ShoutOut')
+  shoutOut(
+    messageInput: any
+  ): IString {
+    this.logger.log('shoutOut' + messageInput.msg)
+    return {
+      message: this.appService.shoutOut(messageInput)
+    }
   }
 }
